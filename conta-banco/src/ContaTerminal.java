063f7fd970980e5556b8c9efd90f90c174b2702a
Eliezer Bittencourt <likobittencourt@gmail.com>
2024-08-31 15:47:58 -0300

    first commit

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+		System.out.println("Por favor, digite seu nome:");
+        String nome = scanner.next();
+
+		System.out.println("Por favor, digite o número da sua agência: ");
+        String agencia = scanner.next();
+
+		System.out.println("Por favor, digite o número da sua conta: ");
+        int numero = scanner.nextInt();
+
+        double saldo = 235.8;
+
+        System.out.println("Olá "+ nome +" , obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta "+ numero +" e seu saldo "+ saldo +" já está disponível para saque");
+    }
+}
