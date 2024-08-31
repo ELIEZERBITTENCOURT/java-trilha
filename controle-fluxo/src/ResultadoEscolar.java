a083f2e5e6d1b28f736282fc0dac5d86fae0180f
Eliezer Bittencourt <likobittencourt@gmail.com>
2024-08-31 18:26:45 -0300

    condicao ternaria

---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,32 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 5;
+
+        if (nota >= 7)
+		    System.out.println("Aprovado");
+	    else if (nota >= 5 && nota < 7)
+		    System.out.println("Recuperação");
+	    else
+		    System.out.println("Reprovado");
+    }
+}
+
+/* Condição Ternária
+ *
+ * public class ResultadoEscolar {
+ *	public static void main(String[] args) {
+ *		int nota = 7;
+ *		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+ *		System.out.println(resultado);
+ *	}
+ * }
+ * 
+ * OU
+ * 
+ * public static void main(String[] args) {
+ *		int nota = 6;
+ *		String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+ *		System.out.println(resultado);
+ *	}
+  *
+ */
