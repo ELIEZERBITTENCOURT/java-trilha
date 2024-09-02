3eca05fe84b66a6e1b0895b34f0532d7aec839ac
Eliezer Bittencourt <likobittencourt@gmail.com>
2024-09-02 11:59:35 -0300

    excecao try-catch

---
+++ controle-fluxo/src/ExemploExcecao.java
@@ -0,0 +1,37 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExemploExcecao {
+    public static void main(String[] args) {
+        // criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        try {
+
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
+
+            System.out.println("Digite seu sobrenome");
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite sua idade");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura");
+            double altura = scanner.nextDouble();
+
+            // imprimindo os dados obtidos pelo usuario
+            System.out.println("Ola, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+            System.out.println("Tenho " + idade + " anos ");
+            System.out.println("Minha altura é " + altura + "cm ");
+        } catch (InputMismatchException  e) {
+            System.out.println("Erro: A idade e altura precisam ser numericos!");
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro inesperado: " + e.getMessage());
+        }
+
+        scanner.close();
+    }
+
+}
